6b1938e879c78aa0a70270bec37964bf954e5735
Anurag Baundwal <abaundwal@gmail.com>
2023-08-24 20:19:23 +0530

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+            <h1>Sticky Notes - A React App</h1>
+        </header>
+    );
+}
+
+export default Header;
