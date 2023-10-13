be5999d1b76a524bd9de4b9d08fa8077b457de11
Anurag Baundwal <abaundwal@gmail.com>
2023-10-13 19:58:36 +0530

    API related updates

--- src/App.js
+++ src/App.js
@@ -3,22 +3,43 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
 import CreateArea from "./components/CreateArea";
-import api from './api.js';
+import { createAPI } from './api.js';
+import axios from 'axios';
 
 function App() {
   const [notes, setNotes] = useState([]);
+
+  const [baseURL, setBaseURL] = useState("default_url");
+
   useEffect(() => {
-    async function fetchData() {
+    const fetchBaseURL = async () => {
       try {
-        const response = await api.get("/notes");
-        setNotes(response.data);
-        console.log("Notes from API:", response.data);  // Debug line
+        const response = await axios.get("https://fc49-2402-3a80-88c-d9ad-cc6d-949f-a6f9-b77e.ngrok.io/getBaseURL");
+        setBaseURL(response.data.baseURL);
       } catch (error) {
-        console.error("Error fetching data: ", error);
+        console.error("Could not fetch baseURL", error);
       }
+    };
+    fetchBaseURL();
+    console.log(baseURL)
+  });
+
+  useEffect(() => {
+    if (baseURL !== "") {
+      const api = createAPI(baseURL);
+      async function fetchData() {
+        try {
+          const response = await api.get("/notes");
+          setNotes(response.data);
+        } catch (error) {
+          console.error("Error fetching data: ", error);
+        }
+      }
+      fetchData();
     }
-    fetchData();
-  }, []);
+  }, [baseURL]);
+
+  const api = createAPI(baseURL);
 
   async function addNote(newNote) {
     const response = await api.post("/notes", newNote);
--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const baseURL = "https://crudcrud.com/api/49c758d7b1a8454cbe0e8f1d622841e5";
-
-export default axios.create({
-  baseURL: baseURL
-});
+export const createAPI = (baseURL) => {
+  return axios.create({
+    baseURL: baseURL
+  });
+};
